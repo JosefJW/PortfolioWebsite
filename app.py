4b7fdccdfd824c2322a05ce1d0ef1ca376a6f038
Josef Wolf <josefwolf591@gmail.com>
2025-01-09 06:06:24 -0600

    Made suitable for Github Pages

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+"""
 from flask import Flask, render_template, request, jsonify
 import smtplib
 
@@ -62,3 +63,4 @@ def submit_form():
 if __name__ == "__main__":
     #app.run(host='0.0.0.0', port=5000)
     app.run(debug=True)
+"""
